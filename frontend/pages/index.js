fbe47dee383d890e7e82f10666b8f1cf928013b3
CsOrlandi <cso.junior1996@gmail.com>
2021-03-02 23:42:33 -0300

    feat: Create Home Screen and retrieve data from prismic

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,26 @@
-function Index() {
+import Prismic from 'prismic-javascript';
+
+function Index(props) {
   return (
     <div>
       <h1>Socks Shop</h1>
       <p>Hello!</p>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
   )
+}
+
+export async function getServerSideProps({ res }) {
+  const client = Prismic.client('https://socksshop.cdn.prismic.io/api/v2');
+
+  const products = await client.query(Prismic.Predicates.at('document.type', 'product'));
+
+  return {
+    props: {
+      date: Date.now(),
+      products: products.results,
+    }
+  }
 }
 
 export default Index;
